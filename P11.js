272c3c86034cca38f9abd1c4bebc2b25ffcfdd3c
aakarsh7599 <32263537+aakarsh7599@users.noreply.github.com>
2018-06-07 21:00:10 +0530

    Update P11.js

--- P11.js
+++ P11.js
@@ -1,7 +1,16 @@
  function string_to_array(str)
  {
   var arr=str.split(' ');
-  document.writeln(arr);
+  //document.writeln(arr);
+   document.writeln("[");
+ for(var i=0; i<arr.length;i++)
+ {
+    document.writeln("\""+arr[i]+"\"");
+	if(i!=arr.length-1)
+	{document.writeln(",");}
+ }
+ document.writeln("]")
+
  }
  
  string_to_array("I am a student")
